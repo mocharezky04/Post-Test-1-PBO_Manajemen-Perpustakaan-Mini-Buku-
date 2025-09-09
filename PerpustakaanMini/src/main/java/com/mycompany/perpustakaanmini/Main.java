0e41ea66a1d41233fcd421f0a960087cde93dcfd
Mochammad Rezky Ramadhan <rrezkyramadhan04@gmail.com>
2025-09-09 19:09:05 +0800

    Update Main.java

--- PerpustakaanMini/src/main/java/com/mycompany/perpustakaanmini/Main.java
+++ PerpustakaanMini/src/main/java/com/mycompany/perpustakaanmini/Main.java
@@ -125,7 +125,7 @@ public class Main {
                     break;
 
                 case 5:
-                    // Exit
+                    // Keluar
                     System.out.println("Terima kasih telah menggunakan program perpustakaan mini");
                     input.close();
                     return;
